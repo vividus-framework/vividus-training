fdf69f931199ce431dacd2191357b709bf7edc91
Darya Sakel <darya_sakel@epam.com>
2024-03-11 16:55:49 +0300

    Baryla

--- gradlew.bat
+++ gradlew.bat
@@ -1,35 +1,92 @@
-@echo off
+@rem
+@rem Copyright 2015 the original author or authors.
+@rem
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
 
-set GRADLE_PROPERTIES=gradle.properties
-set ERROR_MSG=%GRADLE_PROPERTIES% file is missing
-if exist %GRADLE_PROPERTIES% goto readproperties
+@if "%DEBUG%"=="" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+@rem This is normally unused
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if %ERRORLEVEL% equ 0 goto execute
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
 goto fail
 
-:readproperties
-call :setpropertyvalue buildSystemVersion , buildSystemVersion
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-:checkpath
-if not exist "%VIVIDUS_BUILD_SYSTEM_HOME%" (
-    call :setpropertyvalue buildSystemRootDir , VIVIDUS_BUILD_SYSTEM_HOME
-)
-set GRADLEW_PATH=%VIVIDUS_BUILD_SYSTEM_HOME%\%buildSystemVersion%\gradlew.bat
-if exist "%GRADLEW_PATH%" goto call
-set ERROR_MSG=Couldn't find %GRADLEW_PATH%. Neither environment variable "VIVIDUS_BUILD_SYSTEM_HOME" is set nor embedded build system is synced
+if exist "%JAVA_EXE%" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:end
+@rem End local scope for the variables with windows NT shell
+if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
-echo.
-echo ERROR: %ERROR_MSG%
-echo Please check Build System guide:
-echo https://github.com/vividus-framework/vividus-build-system
-echo or
-echo clone this repo recursively: git clone --recursive <git-repository-url>
-exit /b 1
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ return code!
+set EXIT_CODE=%ERRORLEVEL%
+if %EXIT_CODE% equ 0 set EXIT_CODE=1
+if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
+exit /b %EXIT_CODE%
 
-:call
-@CALL "%GRADLEW_PATH%" %*
-exit /b %ERRORLEVEL%
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
 
-:setpropertyvalue
-For /F "tokens=1* delims==" %%A IN (%GRADLE_PROPERTIES%) DO (
-    IF %%A==%~1 set %~2=%%B
-)
+:omega
--- src/main/resources/story/training/Task_3.story
+++ src/main/resources/story/training/Task_3.story
@@ -4,7 +4,7 @@ When I take screenshot of Homepage
 
 Scenario: Log in as a Good User
 When I login with ${userName} and ${password}
-When I wait until element located by `xpath(//*[@id="header_container"]/div[1]/div[2]/div)` appears
+When I wait until element located by `xpath(//*[@id="header_container"])` appears
 Then `${current-page-url}` is equal to `https://www.saucedemo.com/inventory.html`
 Then number of elements found by `className(inventory_item)` is equal to `6`
 When I take screenshot
